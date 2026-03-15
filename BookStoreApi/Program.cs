bb25a81cc0c0b3535d0a71e8ac402cbdd7fcc623
Khadijatou03 <108757881+Khadijatou03@users.noreply.github.com>
2026-03-15 00:42:44 +0000

    Premier commit

---
+++ BookStoreApi/Program.cs
@@ -0,0 +1,45 @@
+
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
+namespace BookStoreApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+            builder.Services.AddSingleton<BooksService>();
+
+            builder.Services.AddControllers()
+                 .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
